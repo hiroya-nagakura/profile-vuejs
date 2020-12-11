698c1d6a105c5af0fa0ff4c6b9b5e43993ad43bb
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2020-12-12 04:20:31 +0900

    ヘッダー骨組み

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav id='header'>
+    <ul>
+      <li v-for="(item, index) in items" :key='index'>{{ item }}</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [ 'このサイトについて', 'ポートフォリオ', 'スキル', '略歴', '問合せ' ]
+    };
+  }
+};
+</script>
