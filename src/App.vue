0d617122597405d6127907cf0c891407a6c4eb87
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-01-05 13:26:22 +0900

    [app]PCヘッダー雛形作成

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,17 @@
 <template>
   <v-app>
-    <header>
-      <v-app-bar
-        app
-        dark>
-        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-        <v-toolbar-title>
-          MyPortfoioSite
-        </v-toolbar-title>
-      </v-app-bar>
-      <v-navigation-drawer
-        v-model='drawer'
-        fixed
-        temporary
-      >
-        <v-list
-          nav
-          dense
-        >
-          <v-list-item-group>
-            <v-list-item
-              v-for='(menuItem, index) in menuItems'
-              :key='index'
-            >
-              <v-list-item-title>{{menuItem.name}}</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
-    </header>
+    <AppHeader/>
   </v-app>
 </template>
 
 <script>
-import constants from './common/constants'
+  import AppHeader from './components/AppHeader.vue'
 
-export default {
-  data() {
-    return {
-      drawer: false,
-      menuItems: constants.menuItems
-    } 
-  }
-}
+  export default {
+    name: 'App',
+    components: {
+      AppHeader
+    }
+  };
 </script>
 
-<style></style>
