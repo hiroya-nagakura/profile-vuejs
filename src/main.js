a107faacb5aec38107ad2c83c8ee32aeb608c3e5
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-01-08 23:25:58 +0900

    [add]ScrollRevealライブラリ導入

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import ScrollReveal from 'scrollreveal'
 import "./styles/common/common.scss";
 
 Vue.config.productionTip = false
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
+  ScrollReveal,
   render: h => h(App)
 }).$mount('#app')
