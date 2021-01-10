762ebac8bdd0c3b0f0d24c9db19ffe75024efe15
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-01-10 23:24:19 +0900

    [update]自己紹介文追加

--- src/components/HomeAbout.vue
+++ src/components/HomeAbout.vue
@@ -1,19 +1,19 @@
 <template>
     <section class="home-about">
       <div class="section__title">
-        <div class="section__title-text">01. About Me</div>
+        <div class="section__title-text">About Me</div>
       </div>
       <v-container>
-        <v-row class="home-about__contents">
-          <v-col md="5" class="home-about__contents-img">
-            <img src="../assets/img/profile.jpg" alt="" class="img-fluid">
-          </v-col>
-          <v-col md="5" class="home-about__contents-text">
-            <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
-            <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+        <v-row class="home-about__contents fade-in">
+          <v-col md="9" class="home-about__contents-text">
+            <p>1993年鹿児島生まれの{{ calcAge }}歳、ヒロヤと申します。</p>
+            <p>鹿児島大学大学院農学研究科卒。デンプンに作用する酵素について研究をしていました。その後、製パン会社に就職し、パン生地の作成を主に担当。</p>
+            <p>毎日の仕事をこなしていく中で、今の仕事を一生続けていくことに限界を感じ以前から興味のあったプログラミングを始めてみる</p>
+            <p>自分の手で一人で作品を作り上げることに楽しさを感じ、学習を進める</p>
+            <p>ASW, Docker, CICLパイプラインなどを盛り込んだポートフォリオを独学で完成させ、現在転職活動中。</p>
           </v-col>
         </v-row>
-        <AppButton :button-text="buttonText" :url='url' />
+        <AppButton :button-text="buttonText" :url='url' class='fade-in'/>
       </v-container>
     </section>
 </template>
@@ -30,6 +30,14 @@ export default {
   },
   components: {
     AppButton
+  },
+  computed: {
+    calcAge() {
+      const myBirtyday = 19931219;
+      const today = new Date();
+      const targetdate = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
+      return  (Math.floor((targetdate - myBirtyday) / 10000));
+    }
   }
 }
 </script>
@@ -38,13 +46,11 @@ export default {
   .home-about__contents {
     margin-top: 20px;
     justify-content: center;
-  
-    &-img {
-      text-align: center;
-    }
-  
+
     &-text {
       word-wrap: break-word;
+      font-size: 23px;
+      color: rgb(95, 95, 95);
     }
   }
 </style>
