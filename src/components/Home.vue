af272feed45286625b7ef8fe58090c65e69811ad
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-01-10 23:23:02 +0900

    [fixed]ScrollReveal登録変更

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,12 +10,21 @@
 import HomeHero from './HomeHero.vue'
 import HomeAbout from './HomeAbout.vue'
 import HomeWork from './HomeWork.vue'
+import ScrollReveal from 'scrollreveal'
 
 export default {
   components: {
     HomeHero,
     HomeAbout,
     HomeWork
+  },
+  mounted() {
+    ScrollReveal().reveal('.fade-in', {
+      duration: 500,
+      viewFactor: 0.5,
+      origin: 'bottom',
+      distance: '100px',
+    });
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
-import ScrollReveal from 'scrollreveal'
 import "./styles/common/common.scss";
 
 Vue.config.productionTip = false
@@ -10,6 +9,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
-  ScrollReveal,
   render: h => h(App)
 }).$mount('#app')
